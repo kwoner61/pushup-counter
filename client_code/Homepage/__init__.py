4ad1e88c9272d4bc4eb599720f1d3c40af365140
kwoner61@gmail.com <kwoner61@gmail.com>
2023-10-26 15:08:47 +0000

    Created LICENSE.txt; Edited Homepage

--- client_code/Homepage/__init__.py
+++ client_code/Homepage/__init__.py
@@ -7,7 +7,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 from ..EntryEdit import EntryEdit
 from datetime import date
-# import threading
+
 
 class Homepage(HomepageTemplate):
   def __init__(self, **properties):
@@ -19,11 +19,10 @@ class Homepage(HomepageTemplate):
     # Any code you write here will run when the form opens.
     entries = anvil.server.call('get_entries')
     self.bar_chart.data = go.Bar(x=entries['dates'], y=entries['counts'])
-    # threading.Timer(2, form_refreshing_data_bindings).start()
+
 
   def form_refreshing_data_bindings(self, **event_args):
     """This method is called when refreshing_data_bindings is called"""
-    print("Refresh data binding")
     self.item['date'] = date.today().isoformat()
     r = app_tables.pushups.get(date=date.today().isoformat())
     self.item['today_count'] = r['count'] if r else 0
@@ -31,7 +30,8 @@ class Homepage(HomepageTemplate):
     self.bar_chart.data = go.Bar(x=entries['dates'], y=entries['counts'])
     self.refresh_data_bindings()
 
+
   def timer_1_tick(self, **event_args):
     """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
-    print("tick")
     self.form_refreshing_data_bindings()
+
